b3db6cb910c2ca7527dff4c40536b52a4e94ef65
eddyiiz <eddyxsayf@gmail.com>
2025-03-22 14:40:09 +0200

    add user repository

---
+++ src/main/java/com/JWTAuth/api/models/User.java
@@ -0,0 +1,27 @@
+package com.JWTAuth.api.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+}
